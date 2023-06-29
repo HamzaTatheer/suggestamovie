ff0fe06361d391ab9d2d38a1607dbfbc3558306c
Hamza Tatheer <hamzatatheer@gmail.com>
2023-06-30 01:22:17 +0500

    corrected

--- knexfile.js
+++ knexfile.js
@@ -23,7 +23,7 @@ module.exports = {
         host: process.env.RDS_HOSTNAME,
         port:  process.env.RDS_PORT,
         user:  process.env.RDS_HOSTNAME,
-        password: process.env.RDS_HOSTNAME,
+        password: process.env.RDS_PASSWORD,
         database: process.env.RDS_DB_NAME
     },
     migrations: {
