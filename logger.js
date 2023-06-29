ceeb5e7873e25985d6223240b5208f34575499a1
Hamza Tatheer <hamzatatheer@gmail.com>
2023-06-29 23:57:23 +0500

    new test

--- logger.js
+++ logger.js
@@ -10,10 +10,10 @@ const logger = createLogger({
       logStreamName: 'general',
       awsOptions: {
         credentials: {
-          accessKeyId: process.env.CLOUDWATCH_ACCESS_KEY_ID,
-          secretAccessKey: process.env.CLOUDWATCH_ACCESS_SECRET_ACCESS_KEY,
+          accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
         },
-        region:process.env.CLOUDWATCH_REGION,
+        region:process.env.AWS_DEPLOY_REGION,
       }
       }),
   ],
