7f6a97feb72ad72c842194f42c717ecadbe0231b
Hamza Tatheer <hamzatatheer@gmail.com>
2023-06-30 02:00:47 +0500

    very basic

--- knexfile.js
+++ knexfile.js
@@ -22,7 +22,7 @@ module.exports = {
     connection: {
         host: process.env.RDS_HOSTNAME,
         port:  process.env.RDS_PORT,
-        user:  process.env.RDS_HOSTNAME,
+        user:  process.env.RDS_USERNAME,
         password: process.env.RDS_PASSWORD,
         database: process.env.RDS_DB_NAME
     },
